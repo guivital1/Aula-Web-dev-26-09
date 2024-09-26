9b40589d23319156d5ebecd05f5a0bc4ab8380c0
Guilherme Vital Rodrigues <guivitalrodrigues@gmail.com>
2024-09-26 11:08:21 -0300

    Avançando

--- script.js
+++ script.js
@@ -34,5 +34,42 @@ console.log(objeto)
 let array = [];
 console.log(array)
 
-let array1 = [];
-console.log(array1)
+// conversao typeof = verifica o tipo da variavel
+
+let numero = "123"
+console.log(typeof numero)
+let numeroconvertido = parseInt(numero)
+console.log(numeroconvertido)
+
+// operadores
+
+let x = 20;
+let y = 10;
+
+// Operadores Aritméticos
+
+console.log(x + y);
+console.log(x - y);
+console.log(x*y);
+console.log(x/y);
+
+//Operadores de comparação
+
+console.log( x == y)
+console.log( x === y);
+console.log (x!=y);
+
+//Operadores lógicos
+console.log(x > y); // maior
+console.log(x < y);// menor
+console.log(x > y && y < 50); // e
+console.log(x > y || y < 50); // ou
+
+// estrutura condicional
+
+//if
+
+let nome = "fiap"
+if(nome == 'fiap'){
+    console.log("Nome correto")
+}
