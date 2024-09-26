07d825ec8a64727a424a8a581601446940618045
Guilherme Vital Rodrigues <guivitalrodrigues@gmail.com>
2024-09-26 10:43:12 -0300

    Criando exemplo 2 e declaração de variaveis

---
+++ script.js
@@ -0,0 +1,36 @@
+//TIPOS DE DADOS E VARIAVEIS
+
+//valor
+var exemplo = "Olá dev";
+console.log(exemplo)
+
+let exemplo2 = "Fiapinho";
+console.log(exemplo2)
+
+const exemplo3 = "Big fiap";
+console.log(exemplo3);
+
+
+//valor undefined
+let valor;
+console.log(valor)
+
+
+//valor nulo
+let valor1 = null;
+console.log(valor1)
+
+//valor booleano
+
+let valor2 = false
+console.log(valor2)
+
+//valor objeto
+let objeto = {};
+console.log(objeto)
+
+// array
+
+let array = [];
+console.log(array)
+
